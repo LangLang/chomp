71fb93c92d116617a920a4e795b4b84a64378608
Rehno Lindeque <errantkid@gmail.com>
2011-06-12 12:08:53 +0200

    Starting to define the basic input parsing functions

--- src/Main.hs
+++ src/Main.hs
@@ -2,5 +2,36 @@ module Main (
     main
 ) where
 
-main = putStrLn "O brave new world that has such people in it."
+import qualified Data.ByteString.Lazy.Char8 as LC
+import Data.Char
+import Control.Monad
+
+-- Syntax tree representation
+
+type SymbolId    = Int
+data Declare     = Atom SymbolId
+                 | Arrow (SymbolId, Expression)
+data EvalDeclare = AssertDecl Declare
+                 | WitnessDecl Declare
+data Query       = ConjunctSelect Declare SymbolId
+data EvalQuery   = Assert Query
+                 | Witness Query
+type Set         = [Expression]
+data Expression  = Query | Declare | Set
+
+-- Parse input language
+
+parseExpr :: (LC.ByteString, Set) -> (LC.ByteString, Set)
+parseExpr (s, r) = (LC.dropWhile isSpace s, r)
+
+parse :: LC.ByteString -> Set
+parse s = snd $ parseExpr (s, [])
+
+-- Main loop
+
+main :: IO ()
+main = do
+  putStrLn "Chomp v0.0.1 for LangLang"
+  liftM parse LC.getContents
+  return ()
 
